6a9c0293903ab481495ba031c15a08d02c7f2c8d
Simon <simon.mollerberg@outlook.com>
2023-03-19 13:53:55 +0100

    .

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,82 +4,39 @@
   </div>
 
   <div>
-
-    <label>
-      <input v-model="name">
-      <div > Name field has an error.</div>
-    </label>
-
-    <div>
-      <label>Username</label>
-      <br>
-      <input
-          v-model="formData.username"
-      >
-    </div>
-    <div>
-      <label>Email</label>
-      <br>
-      <input
-          type="text"
-          v-model="formData.email"
-      >
-    </div>
-    <div>
-      <label>Password</label>
-      <br>
-      <input
-          type="text"
-          v-model="formData.password"
-      >
-    </div>
-    <div>
-      <label>Confirm Password</label>
-      <br>
-      <input
-          type="text"
-          v-model="formData.confirmPassword"
-      >
-    </div>
+    
+    <input v-model="name">
+    <p v-if="v$.name.$error">Name field has an error.</p>
+    
   </div>
 
-  <button
-      @click="submitForm"
-  >Submit
-  </button>
+<!--  <button-->
+<!--      @click="submitForm"-->
+<!--  >Submit-->
+<!--  </button>-->
 </template>
 
-<script setup>
-import useVuelidate from "@vuelidate/core";
-import {required} from "@vuelidate/validators";
-import {reactive} from "vue";
+<script>
 
-const formData = reactive({
-  name: "",
-  username: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-});
+import {useVuelidate} from '@vuelidate/core'
+import {required} from '@vuelidate/validators'
 
-const rules = {
-  name: {required},
-  username: {required},
-  email: {required},
-  password: {required},
-  confirmPassword: {required},
-};
-
-const v$ = useVuelidate(rules, formData);
-const submitForm = async () => {
-  const result = await v$.value.validate;
-  if (result) {
-    alert("Success!")
-  } else {
-    alert("Failed!")
+export default {
+  
+  setup() {
+    return {
+      v$: useVuelidate()
+    }
+  },
+  data: () => ({
+    name: ''
+  }),
+  validations() {
+    return {
+      name: {required}
+    }
   }
 }
-
 
 </script>
 <style scoped>
